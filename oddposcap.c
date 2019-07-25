e2cddff1d49c98defcde6af92e4583653978aa43
kavin45 <52598647+kavin45@users.noreply.github.com>
2019-07-25 15:48:00 +0530

    Add files via upload

---
+++ oddposcap.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+
+    
+int i,n,len=0;
+    
+char str[100];
+    
+scanf("%s",&str);
+    
+len=strlen(str);
+    
+for(i=0;i<=len;i++)
+    
+{
+        
+if(i%2==0)
+        
+str[i]=str[i]-32;
+    
+}
+    
+printf("%s",str);
+
+    
+return 0;
+
+}
